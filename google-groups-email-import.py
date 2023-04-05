e5715265d81a581999beba08ffe72abc6bb99408
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2023-04-05 15:38:39 -0500

    update

--- google-groups-email-import.py
+++ google-groups-email-import.py
@@ -2,15 +2,39 @@
 import argparse
 import logging
 import mailbox
+import os
 import sys
 
 from google.oauth2 import service_account
 from googleapiclient import discovery
+from googleapiclient.errors import HttpError, MediaUploadSizeError
 from multiprocessing import Process, Queue
 from pathlib import Path
-from time import time, sleep
+from time import time, sleep, perf_counter
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)-23s %(levelname)s %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)-23s %(levelname)s %(message)s"
+)
+
+
+class Timer:
+    def __enter__(self):
+        self.start = perf_counter()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.elapsed = perf_counter() - self.start
+
+    def __str__(self):
+        return str(self.elapsed)
+
+    def __format__(self, fmt):
+        return self.elapsed.__format__(fmt)
+
+
+class WorkingDirectoryNotEmpty(Exception):
+    pass
+
 
 class RateLimiter:
     """Helper to rate-limit request"""
@@ -23,47 +47,117 @@ class RateLimiter:
 
     def wait_for_clearance(self):
         """Wait if rate is too high, and then register time of new request"""
-        now = time()
-        while len(self.hist)/self.interval >= self.max_rate:
+        while len(self.hist) / self.interval >= self.max_rate:
+            now = time()
             self.hist = [t for t in self.hist if now - t <= self.interval]
-            sleep(min(self.hist) + self.interval - now)
-    
+            if self.hist:
+                sleep(min(self.hist) + self.interval - now)
+
     def register(self):
         """Register time of new request"""
         self.hist.append(time())
 
+    def current_rate(self):
+        return len(self.hist) / self.interval
 
-def worker(work_q, feedback_q, group, creds, delegator):
+
+def worker(work_q, feedback_q, ready_q, backoff_q, group, creds, delegator):
     credentials = service_account.Credentials.from_service_account_file(
         creds,
         scopes=["https://www.googleapis.com/auth/apps.groups.migration"],
         subject=delegator,
     )
-    service = discovery.build("groupsmigration", "v1", credentials=credentials)
+    service = discovery.build(
+        "groupsmigration", "v1", credentials=credentials, cache_discovery=False
+    )
     archive = service.archive()
+    pid = os.getpid()
 
     while True:
+        ready_q.put(True)
         msg_file = work_q.get()
+        ready_q.get()
         if msg_file is None:
             return
-        req = archive.insert(
-            groupId=args.dst_group,
-            media_body=msg_file,
-            media_mime_type="message/rfc822",
-        )
-        res = req.execute()
-        feeback_q.put((msg_file, res))
+        try:
+            req = archive.insert(groupId=group, media_body=msg_file, media_mime_type="message/rfc822")
+        except MediaUploadSizeError:
+            logging.info(f"{pid} {msg_file} is bigger than maximum allowed size")
+            feedback_q.put((False, msg_file))
+            continue
+
+        num_retries = 0
+        max_retries = 5
+        while True:
+            sleep(2**num_retries - 1)
+            try:
+                with Timer() as timer:
+                    res = req.execute()
+            except HttpError as e:
+                logging.info(f"{pid} caught HttpError {repr(e)}")
+                if e.status_code == 503:
+                    perform_retry = True
+                    import_success = False
+                else:
+                    perform_retry = False
+                    import_success = False
+            except:
+                logging.info(f"{pid} caught exception {repr(e)}")
+                perform_retry = False
+                import_success = False
+            else:
+                if res['responseCode'] == 'SUCCESS':
+                    logging.info(f"{pid} inserted {msg_file} in {timer:.2f}s {num_retries} retries")
+                    perform_retry = False
+                    import_success = True
+                else:
+                    logging.info(f"{pid} failed to insert {msg_file} {res}")
+                    perform_retry = True
+                    import_success = False
+            
+            if perform_retry:
+                num_retries += 1
+                if num_retries == 1:
+                    backoff_q.put(True)
+                if num_retries <= max_retries:
+                    continue
+                else:
+                    logging.info(f"{pid} giving up on {msg_file} after {num_retries} attempts")
+                    backoff_q.get()
+                    feedback_q.put((import_success, msg_file))
+                    break
+            else:
+                if num_retries:
+                    backoff_q.get()
+                feedback_q.put((import_success, msg_file))
+                break
+
+
+def unpack_mbox(mbox_path, workdir_path):
+    workdir = Path(workdir_path)
+    workdir.mkdir(exist_ok=True)
+    if list(workdir.iterdir()):
+        raise WorkingDirectoryNotEmpty
+
+    mbox = mailbox.mbox(mbox_path)
+    for key in mbox.iterkeys():
+        msg_file = workdir / str(key)
+        msg_file.write_bytes(mbox.get_bytes(key))
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="%(prog)s is a utility to import email messagges from a mailbox\n"
-        "in mbox format into a Google Group archive.",
-        epilog="Notes:\n"
-        "[1] The service account needs to be set up for domain-wide delegation.\n"
-        "[2] The delegator account needs to have a Google Workspace admin role.\n"
-        "\nAlso note that importing the same message (same Message-ID) multiple\n"
-        "times will not result in duplicates.",
+        description=(
+            "%(prog)s is a utility to import email messagges from a mailbox\n"
+            "in mbox format into a Google Group archive."
+        ),
+        epilog=(
+            "Notes:\n"
+            "[1] The service account needs to be set up for domain-wide delegation.\n"
+            "[2] The delegator account needs to have a Google Workspace admin role.\n"
+            "\nAlso note that importing the same message (same Message-ID) multiple\n"
+            "times will not result in duplicates."
+        ),
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
@@ -91,54 +185,84 @@ def main():
         help="destination group ID",
     )
     parser.add_argument(
-        "--num-workers",
-        metavar="NUM",
-        default=8,
-        help="number of workers",
-    )
-    parser.add_argument(
         "--work-dir",
         metavar="PATH",
         default="./workdir",
-        help="storage for unpacked mailbox",
+        help="storage for unpacked mailbox (default: ./workdir)",
+    )
+    parser.add_argument(
+        "--resume",
+        action="store_true",
+        help="resume using previously unpacked mailbox",
     )
     args = parser.parse_args()
 
-    workdir = Path(args.work_dir)
-    workdir.mkdir(exist_ok=True)
-    if list(workdir.iterdir()):
-        parser.exit(1, "Working directory not empty")
+    if not args.resume:
+        try:
+            unpack_mbox(args.src_mbox, args.work_dir)
+        except WorkingDirectoryNotEmpty:
+            parser.exit(1, "Error: working directory is not empty but --resume not given")
+    msg_files = [str(f) for f in Path(args.work_dir).iterdir()]
+    total_files = len(msg_files)
+    processed_files = 0
+    logging.info(f"{total_files} messages to work on")
 
-    mbox = mailbox.mbox(args.src_mbox)
-    for key, msg in mbox.iteritems():
-        msg_file = workdir / str(key)
-        msg_file.write_text(str(msg))
+    work_q = Queue()  # message file names for workers to work on (input)
+    feedback_q = Queue()  # message file names from workers after they've been imported
+    ready_q = Queue()  # qlen is the number of workers ready and waiting for work
+    backoff_q = Queue()  # qlen is the number of workers retransmitting and backing off
 
+    NUM_PROCS = 10
+    args1 = (work_q, feedback_q, ready_q, backoff_q)
+    args2 = (args.dst_group, args.sa_creds, args.sa_delegator)
+    procs = [Process(target=worker, args=args1 + args2) for i in range(NUM_PROCS)]
+    [p.start() for p in procs]
 
-    return
-    credentials = service_account.Credentials.from_service_account_file(
-        args.sa_creds,
-        scopes=["https://www.googleapis.com/auth/apps.groups.migration"],
-        subject=args.sa_delegator,
-    )
+    MAX_REQ_RATE = 10
+    ratelimiter = RateLimiter(MAX_REQ_RATE, 1)
+    while True:
+        if msg_files and ready_q.qsize() and work_q.empty() and backoff_q.empty():
+            ratelimiter.wait_for_clearance()
+            ratelimiter.register()
+            work_q.put(str(msg_files.pop(0)))
+            # logging.info(f"current rate: {ratelimiter.current_rate()}")
+        while feedback_q.qsize():
+            success, done_msg_file = feedback_q.get()
+            if success:
+                Path(done_msg_file).unlink()
+            processed_files += 1
+        if processed_files == total_files:
+            break
+        sleep(0.01)
 
-    service = discovery.build("groupsmigration", "v1", credentials=credentials)
-    archive = service.archive()
+    [work_q.put(None) for p in procs]
+    [p.join() for p in procs]
 
-    req = archive.insert(
-        groupId=args.dst_group,
-        media_body=XXX,
-        media_mime_type="message/rfc822",
-    )
-    res = req.execute()
-    service.close()
-
-    # try:
-    #    response = request.execute()
-    # except HttpError as e:
-    #    print('Error response status code : {0}, reason : {1}'.format(e.status_code, e.error_details))
-
-    # print(json.dumps(response, sort_keys=True, indent=4))
+#    credentials = service_account.Credentials.from_service_account_file(
+#        args.sa_creds,
+#        scopes=["https://www.googleapis.com/auth/apps.groups.migration"],
+#        subject=args.sa_delegator,
+#    )
+#    service = discovery.build(
+#        "groupsmigration", "v1", credentials=credentials, cache_discovery=False
+#    )
+#    archive = service.archive()
+#
+#    for fn in msg_files:
+#        req = archive.insert(
+#            groupId=args.dst_group,
+#            media_body=fn,
+#            media_mime_type="message/rfc822",
+#        )
+#        t0 = time()
+#        try:
+#            res = req.execute()
+#        except:
+#            print('hello')
+#        logging.info(
+#           f"{os.getpid()}: Request executed in {round(time() - t0, 2)}s "
+#        )
+#    return
 
 
 if __name__ == "__main__":
